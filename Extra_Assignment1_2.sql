057e4e695dc4d5c1969f3eb87eeecc3fa4261f32
trungdo111198 <89589520+trungdo111198@users.noreply.github.com>
2021-08-29 16:48:28 +0700

    Add files via upload

---
+++ Extra_Assignment1_2.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS Fresher;
+CREATE DATABASE IF NOT EXISTS Fresher;
+USE Fresher;
+CREATE TABLE Trainee(
+	TraineeID 			MEDIUMINT UNSIGNED PRIMARY KEY,
+    Full_name 			NVARCHAR(50) NOT NULL,
+    Birth_date 			DATE NOT NULL,
+    Gender 				ENUM('male','female','unknown'),
+    ET_IQ 				TINYINT UNSIGNED CHECK (ET_IQ<=20),
+    ET_Gmath 			TINYINT UNSIGNED CHECK(ET_Gmath<= 20),
+    ET_English 			TINYINT UNSIGNED CHECK(ET_English<=50),
+    Training_Class 		VARCHAR(50) NOT NULL,
+    Evaluation_Note 	TEXT,
+    VTI_Account 		VARCHAR(50) NOT NULL UNIQUE
+);
+CREATE TABLE Data_Type1(
+	ID 					MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `Name` 				VARCHAR(50) NOT NULL,
+    `Code` 				CHAR(5),
+    ModifiedDate 		DATETIME
+);
+CREATE TABLE Data_Type2(
+	ID 					MEDIUMINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    `Name` 				VARCHAR(50) NOT NULL,
+    BirthDate 			DATE,
+    Gender 				ENUM ('0','1','Unknown'),
+    IsDeletedFlag		ENUM ('0','1')
+);
